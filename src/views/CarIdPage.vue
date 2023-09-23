1135f7d94f8f544d73419878a50ece74c9ca1743
BroMaster132 <emirmatov@gmail.com>
2023-09-23 21:06:53 +0600

    Update

--- src/views/CarIdPage.vue
+++ src/views/CarIdPage.vue
@@ -7,16 +7,16 @@
             <template #header>
                 <img style="width: 350px;" :src="auto.image" alt="car" class="car-image">
             </template>
-            <template #title>{{auto.brand}}</template>
+            <template #title>{{autoRemake.brand}}</template>
             <template #content>
-            <p>Цена: {{auto.price }}</p>
-            <p>Год выпуска: {{formatDate(auto.year)}}</p>
-            <p>Объем двигателя: {{auto.volume}}</p>
+            <p>Цена: {{autoRemake.price }}</p>
+            <p>Год выпуска: {{autoRemake.year}}</p>
+            <p>Объем двигателя: {{autoRemake.volume}}</p>
             Цвет:
-            <ColorPicker disabled v-model="auto.color" />
-            <p>Город: {{auto.city }}</p>
-            <p>Коробка передачи: {{auto.gear}}</p>
-            <p>Кузов: {{auto.carcase }}</p>
+            <ColorPicker disabled v-model="autoRemake.color" />
+            <p>Город: {{autoRemake.city }}</p>
+            <p>Коробка передачи: {{autoRemake.gear}}</p>
+            <p>Кузов: {{autoRemake.carcase }}</p>
             </template>
         </Card>
     </div>
@@ -29,6 +29,7 @@ import { useRoute } from 'vue-router';
 import { formatDate } from "@/services/methods"
 import Card from 'primevue/card';
 import Skeleton from 'primevue/skeleton'
+import ColorPicker from 'primevue/colorpicker';
 const route = useRoute()
 
 
@@ -38,6 +39,17 @@ onMounted(async () =>{
     await getAuto(route.params.id)
 })
 
+const autoRemake = computed(() =>{
+    console.log(auto);
+    return {
+        ...auto.value,
+        price : `${auto.value.price} KZT`,
+        volume : `${auto.value.volume} л`,
+        year : formatDate(auto.value.year) + ' г',
+    }
+})
+
+console.log(autoRemake);
 
 </script>
 
